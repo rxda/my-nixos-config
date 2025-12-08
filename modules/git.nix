479126a1f2b96d86b61fe39a3138ea3e5b66de67
RXDA <sxfscool@gmail.com>
2025-12-08 15:53:29 +0800

    update

--- modules/git.nix
+++ modules/git.nix
@@ -3,11 +3,12 @@
 {
   programs.git = {
     enable = true;
-    userName = "RXDA";
-    userEmail = "sxfscool@gmail.com";
-    
     # 你可以在这里加一些全局配置，比如默认分支名
-    extraConfig = {
+    settings = {
+      user = {
+        name = "RXDA";
+        email = "sxfscool@gmail.com";
+      };
       init.defaultBranch = "main";
       pull.rebase = true;
     };
