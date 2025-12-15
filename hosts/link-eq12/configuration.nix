6a13d67ff30c3d33b26ec6957deefe470ee5cf13
RXDA <sxfscool@gmail.com>
2025-12-15 23:42:19 +0800

    feat: eq12

---
+++ hosts/link-eq12/configuration.nix
@@ -0,0 +1,54 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ 
+      ./hardware-configuration.nix
+      
+      # 引入拆分后的系统模块
+      # ../../system/nvidia.nix
+      ../../system/desktop.nix
+      ../../system/virtualization.nix
+      ../../system/services.nix
+      ../../system/variables.nix
+    ];
+
+  # --- 引导与内核 ---
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # --- 网络标识 ---
+  networking.hostName = "link-eq12"; 
+  networking.networkmanager.enable = true;
+
+  # --- 时区与语言 ---
+  time.timeZone = "Asia/Shanghai";
+  i18n.defaultLocale = "zh_CN.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "zh_CN.UTF-8";
+    LC_IDENTIFICATION = "zh_CN.UTF-8";
+    LC_MEASUREMENT = "zh_CN.UTF-8";
+    LC_MONETARY = "zh_CN.UTF-8";
+    LC_NAME = "zh_CN.UTF-8";
+    LC_NUMERIC = "zh_CN.UTF-8";
+    LC_PAPER = "zh_CN.UTF-8";
+    LC_TELEPHONE = "zh_CN.UTF-8";
+    LC_TIME = "zh_CN.UTF-8";
+  };
+
+  # --- 用户配置 ---
+  users.users.rxda = {
+    isNormalUser = true;
+    description = "rxda";
+    extraGroups = [ "networkmanager" "wheel" "docker" "libvirtd" ];
+  };
+  users.defaultUserShell = pkgs.zsh;
+  programs.zsh.enable = true;
+
+  # --- Nix 核心设置 ---
+  nixpkgs.config.allowUnfree = true;
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  
+  # 系统版本 (千万别删)
+  system.stateVersion = "25.11"; 
+}
