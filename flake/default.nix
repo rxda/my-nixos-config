b2809c99592fb24053060946a3c40bf523881de5
RXDA <sxfscool@gmail.com>
2025-12-18 17:30:23 +0800

    feat: flake parts

--- flake/default.nix
+++ flake/default.nix
@@ -8,7 +8,15 @@
       specialArgs = { inherit inputs; };
       # 模块列表变得极其干净
       modules = [
+        ./common.nix
         ../hosts/xiaomi-notebook/configuration.nix
+        ../hosts/xiaomi-notebook/hardware-configuration.nix
+        ../system/nvidia.nix
+        ../system/desktop.nix
+        ../system/virtualization.nix
+        ../system/services.nix
+        ../system/variables.nix
+        ../system/singbox.nix
       ];
     };
 
@@ -16,7 +24,14 @@
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
       modules = [
+        ./common.nix
         ../hosts/link-eq12/configuration.nix
+        ../hosts/link-eq12/hardware-configuration.nix
+        ../system/desktop.nix
+        ../system/virtualization.nix
+        ../system/services.nix
+        ../system/variables.nix
+        ../system/singbox.nix
       ];
     };
   };
