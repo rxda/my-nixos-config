fbc73a361890e141bf6befd76c31103676b1ef28
RXDA <sxfscool@gmail.com>
2025-12-18 20:47:33 +0800

    feat: disable-hibernate

---
+++ system/disable-hibernate.nix
@@ -0,0 +1,10 @@
+{ pkgs, inputs, ... }: 
+
+{
+  # 永不休眠配置
+  systemd.targets.sleep.enable = false;
+  systemd.targets.suspend.enable = false;
+  systemd.targets.hibernate.enable = false;
+  systemd.targets.hybrid-sleep.enable = false;
+
+}
