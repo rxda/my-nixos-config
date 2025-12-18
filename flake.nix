e05bc402a30df903a6ffee71e7c4b74f89a444bc
RXDA <sxfscool@gmail.com>
2025-12-18 21:11:19 +0800

    feat: gateway

--- flake.nix
+++ flake.nix
@@ -10,7 +10,6 @@
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     agenix.url = "github:ryantm/agenix";
     vscode-server.url = "github:nix-community/nixos-vscode-server";
-    daeuniverse.url = "github:daeuniverse/flake.nix";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
