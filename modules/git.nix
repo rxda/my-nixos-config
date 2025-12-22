5f25a50962d15b80837ba16639918b8a5a7aebee
RXDA <sxfscool@gmail.com>
2025-12-22 09:33:31 +0800

    feat: commit sign

--- modules/git.nix
+++ modules/git.nix
@@ -3,20 +3,25 @@
 {
   programs.git = {
     enable = true;
+    signing = {
+        key = "~/.ssh/id_ed25519.pub";
+        signByDefault = true;
+    };
     # 你可以在这里加一些全局配置，比如默认分支名
     settings = {
       user = {
         name = "RXDA";
         email = "sxfscool@gmail.com";
       };
-      signing = {
-        key = "~/.ssh/id_ed25519.pub";
-        signByDefault = true;
-      };
       init.defaultBranch = "main";
       pull.rebase = true;
+      push.autoSetupRemote = true;
       gpg.format = "ssh";
-      
     };
+  };
+
+  programs.ssh = {
+    enable = true;
+    addKeysToAgent = "yes";
   };
 }
