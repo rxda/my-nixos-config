6f2b4584a87271c13d0729843a2f4244838f1d02
RXDA <sxfscool@gmail.com>
2026-01-12 21:35:29 +0800

    feat: niri

--- flake.nix
+++ flake.nix
@@ -41,6 +41,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    noctalia = {
+      url = "github:noctalia-dev/noctalia-shell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     my-templates = {
       url = "github:rxda/my-nix-templates";
       inputs.nixpkgs.follows = "nixpkgs";
