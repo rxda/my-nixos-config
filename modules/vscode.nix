68ac936ebce776dd5b171c6b484c596de642062c
RXDA <sxfscool@gmail.com>
2026-04-27 17:38:05 +0800

    feat: remove some vscode ext

--- modules/vscode.nix
+++ modules/vscode.nix
@@ -4,12 +4,14 @@
 
   home.packages = [
     pkgs.nixd
-    pkgs.nixpkgs-fmt
+    pkgs.nixfmt-rfc-style
   ];
 
   programs.vscode = {
     enable = true;
     mutableExtensionsDir = false;
+    enableExtensionUpdateCheck = false;
+    enableUpdateCheck = false;
 
     profiles.default.userSettings = {
       # 开启自动保存，建议使用 afterDelay
@@ -23,7 +25,7 @@
       "nix.serverSettings" = {
         "nixd" = {
           "formatting" = {
-            "command" = [ "nixpkgs-fmt" ];
+            "command" = [ "nixfmt" ];
           };
           "options" = {
             # 这里的配置是 nixd 的灵魂，它能让你在写 Nix 选项时获得补全
@@ -45,21 +47,15 @@
     # 注意：这一大坨扩展列表
     profiles.default.extensions = with pkgs; [
       vscode-marketplace.akamud.vscode-theme-onedark
-      vscode-marketplace.arrterian.nix-env-selector
+      # vscode-marketplace.arrterian.nix-env-selector
       vscode-marketplace.arturodent.find-and-transform
-      vscode-marketplace.bierner.markdown-mermaid
-      vscode-marketplace.bpruitt-goddard.mermaid-markdown-syntax-highlighting
       vscode-marketplace.clinyong.vscode-css-modules
       vscode-marketplace.dlahmad.dracula-for-rust-theme
-      vscode-marketplace.docker.docker
-      vscode-marketplace.emilast.logfilehighlighter
-      vscode-marketplace.eriklynd.json-tools
       vscode-marketplace.esbenp.prettier-vscode
       vscode-marketplace.everettjf.filter-line
       vscode-marketplace.fill-labs.dependi
       vscode-marketplace.golang.go
       vscode-marketplace.ianic.zig-language-extras
-      vscode-marketplace.ibm.output-colorizer
       vscode-marketplace.iliazeus.vscode-ansi
       vscode-marketplace.irongeek.vscode-env
       vscode-marketplace.isudox.vscode-jetbrains-keybindings
@@ -70,35 +66,18 @@
       vscode-marketplace.marlon407.code-groovy
       vscode-marketplace.mechatroner.rainbow-csv
       vscode-marketplace.mkhl.direnv
-      vscode-marketplace.ms-azuretools.vscode-containers
-      vscode-marketplace.ms-azuretools.vscode-docker
-      vscode-marketplace.ms-python.autopep8
-      vscode-marketplace.ms-python.debugpy
-      vscode-marketplace.ms-python.isort
-      vscode-marketplace.ms-python.python
-      vscode-marketplace.ms-python.vscode-pylance
-      vscode-marketplace.ms-python.vscode-python-envs
-      vscode-marketplace.ms-toolsai.jupyter
-      vscode-marketplace.ms-toolsai.jupyter-keymap
-      vscode-marketplace.ms-toolsai.jupyter-renderers
-      vscode-marketplace.ms-toolsai.vscode-jupyter-cell-tags
-      vscode-marketplace.ms-toolsai.vscode-jupyter-slideshow
       vscode-marketplace.ms-vscode-remote.remote-ssh
       vscode-marketplace.ms-vscode-remote.remote-ssh-edit
       vscode-marketplace.ms-vscode.cmake-tools
       vscode-marketplace.ms-vscode.cpptools
       vscode-marketplace.ms-vscode.hexeditor
       vscode-marketplace.ms-vscode.remote-explorer
-      vscode-marketplace.ms-vscode.vscode-typescript-next
       vscode-marketplace.naco-siren.gradle-language
-      vscode-marketplace.quicktype.quicktype
+      # vscode-marketplace.quicktype.quicktype
       vscode-marketplace.randomfractalsinc.geo-data-viewer
       vscode-marketplace.redhat.vscode-xml
       vscode-marketplace.rust-lang.rust-analyzer
-      vscode-marketplace.shakram02.bash-beautify
-      vscode-marketplace.tabbyml.vscode-tabby
       vscode-marketplace.tamasfe.even-better-toml
-      vscode-marketplace.tobias-faller.vt100-syntax-highlighting
       vscode-marketplace.twxs.cmake
       # vscode-marketplace-universal.vadimcn.vscode-lldb  
       vscode-marketplace.vscode-icons-team.vscode-icons
@@ -107,12 +86,13 @@
       vscode-marketplace.wayou.vscode-todo-highlight
       vscode-marketplace.yangdada.vscode-geojsonviewer
       vscode-marketplace.yzhang.markdown-all-in-one
-      vscode-marketplace.zainchen.json
       vscode-marketplace.ziglang.vscode-zig
       vscode-marketplace.github.github-vscode-theme
       vscode-marketplace.quicktype.quicktype
       vscode-marketplace.biomejs.biome
       vscode-marketplace.bradlc.vscode-tailwindcss
+      vscode-marketplace.rooveterinaryinc.roo-cline
+      vscode-marketplace.anthropic.claude-code
       vscode-extensions.vadimcn.vscode-lldb
     ];
   };
